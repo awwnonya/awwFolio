10937903ff2f42b282ee279bc5b012f73c056f5d
Ananya Pal <palananya08@gmail.com>
2023-04-20 00:46:19 +0530

    ff

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //Swiper JS (testimonial section)
 const swiper = new Swiper('.swiper', {
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
-  });
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
 
 
 const menuBtn = document.querySelector("#menu-btn");
@@ -13,34 +13,33 @@ const menu = document.querySelector("nav .container ul");
 
 //show menu
 menuBtn.addEventListener('click', () => {
-  menu.style.display = 'block';
-  menuBtn.style.display = 'none';
-  closeBtn.style.display = 'inline-block';
+menu.style.display = 'block';
+menuBtn.style.display = 'none';
+closeBtn.style.display = 'inline-block';
 })
 
 //hide menu
 closeBtn.addEventListener('click', () => {
-  menu.style.display = 'none';
-  menuBtn.style.display = 'inline-block';
-  closeBtn.style.display = 'none';
+menu.style.display = 'none';
+menuBtn.style.display = 'inline-block';
+closeBtn.style.display = 'none';
 })
-
 
 const navItems = menu.querySelectorAll('li');
 
 const changeActiveIcon = () => {
-  navItems.forEach(item => {
-    const link =item.querySelector('a');
-    link.classList.remove('active');
-  })
+navItems.forEach(item => {
+  const link =item.querySelector('a');
+  link.classList.remove('active');
+})
 }
 
 navItems.forEach(item => {
-  const link = item.querySelector('a');
-  link.addEventListener('click', () => {
-    changeActiveIcon();
-    link.classList.add('active');
-  })
+const link = item.querySelector('a');
+link.addEventListener('click', () => {
+  changeActiveIcon();
+  link.classList.add('active');
+})
 })
 
 //read more about
@@ -48,29 +47,29 @@ const readMoreBtn = document.querySelector('.read-more');
 const readMoreContent = document.querySelector('.read-more-content');
 
 readMoreBtn.addEventListener('click', () => {
-  readMoreContent.classList.toggle('show-content');
-  if(readMoreContent.classList.contains('show-content')) {
-    readMoreBtn.textContent = "Show less";
-  }
-  else {
-    readMoreBtn.textContent = "Show more";
-  }
+readMoreContent.classList.toggle('show-content');
+if(readMoreContent.classList.contains('show-content')) {
+  readMoreBtn.textContent = "Show less";
+}
+else {
+  readMoreBtn.textContent = "Show more";
+}
 })
-
-//CIRCLE ROTATION
-let circ = new CircleType(document.getElementById('circ'));
-circ.raduis(360);
 
 // show/hide skills
 const skillItems = document.querySelectorAll('section.skills .skill');
 
 skillItems.forEach(skill => {
-  skill.querySelector('.head').addEventListener('click', () => {
-    skill.querySelector('.items').classList.toggle('show-items');
-  })
+skill.querySelector('.head').addEventListener('click', () => {
+  skill.querySelector('.items').classList.toggle('show-items');
+})
 })
 
 //add box shadow on scroll
 window.addEventListener('scroll', () => {
-  document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
+document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
 })
+
+//CIRCLE ROTATION
+let circ = new CircleType(document.getElementById('circ'));
+circ.raduis(360);
